0b460bf18c08c26af65a570a34af942b8afdb7df
Youngjoon Lee <taxihighway@gmail.com>
2023-04-27 11:55:00 +0900

    fix: set ignorecase=false

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    ignorecase = false, -- sets vim.opt.ignorecase
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
