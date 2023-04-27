c810191b74bfe395eb454d5590c0721ded40f053
Youngjoon Lee <taxihighway@gmail.com>
2023-04-27 15:52:00 +0900

    fix: prevent vimdiff from opening files with readonly

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     ignorecase = false, -- sets vim.opt.ignorecase
+    readonly = false, -- sets vim.opt.readonly
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
